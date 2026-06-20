3f6e794f5c4ca17534ea9da777d1d4db035f5dbd
killuao0 <yashrajsingh111008@gmail.com>
2026-06-20 11:03:26 +0530

    Maxnumber

--- src/controlflow/Maxnumber.java
+++ src/controlflow/Maxnumber.java
@@ -1,4 +1,16 @@
 package controlflow;
 
-public class Maxinumber {
+public class Maxnumber {
+    public static void main(String[] args) {
+        int a = 12, b = 15, c = 9;
+        if (a>b){
+            if (a>c){
+                System.out.println("max number " + a);}
+        }else if (c>a){
+            if (c>b){
+                System.out.println("max number " +c);}
+        }else{
+            System.out.println("max number " +b);}
+
+    }
 }
